0867fb054737c267ec39f119edd9b4f83d80a0cc
netizen-0 <rishabhytpremium17@gmail.com>
2025-12-01 21:00:28 +0530

    Update __main__.py

--- RAUSHAN/__main__.py
+++ RAUSHAN/__main__.py
@@ -9,11 +9,11 @@ from RAUSHAN.modules import ALL_MODULES
 async def start_bot():
     for all_module in ALL_MODULES:
         importlib.import_module("RAUSHAN.modules." + all_module)
-    LOG.print("[bold yellow]𝗛𝗔𝗖𝗞 𝐁𝐎𝐓 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 𝐍𝐎𝐖 𝗙𝗨𝗖𝗞 𝗔𝗟𝗟 𝗧𝗚 𝗜𝗗")
+    LOG.print("[bold red]𝗛𝗔𝗖𝗞 𝐁𝐎𝐓 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 𝐍𝐎𝐖 𝗙𝗨𝗖𝗞 𝗔𝗟𝗟 𝗧𝗚 𝗜𝗗")
     await idle() 
-    LOG.print("[bold red]𝐂𝐀𝐍𝐂𝐋𝐄 𝐀𝐋𝐋 𝐓𝐀𝐒𝐊🤐..........")
+    LOG.print("[bold yellow]𝐂𝐀𝐍𝐂𝐋𝐄 𝐀𝐋𝐋 𝐓𝐀𝐒𝐊🤐..........")
 
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     asyncio.get_event_loop().run_until_complete(start_bot())
